ea61302200974fc117727aa7ed41eb015b4f93b1
lilnoggi <147061927+lilnoggi@users.noreply.github.com>
2025-06-16 21:45:49 +0100

    Functional Buttons
    Main menu is basically done.

---
+++ Assets/Scripts/Managers/MainMenuUIManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUIManager : MonoBehaviour
+{
+    [Header("Panels")]
+    public GameObject mainMenuPanel;
+    public GameObject optionsPanel;
+    public GameObject levelSelectPanel;
+    public GameObject creditsPanel;
+
+    public void OnPlayButton()
+    {
+        // Load game scene here (replace with actual scene name)
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void OnOptionsButton()
+    {
+        mainMenuPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    public void OnLevelSelectButton()
+    {
+        mainMenuPanel.SetActive(false);
+        levelSelectPanel.SetActive(true);
+    }
+
+    public void OnCreditsButton()
+    {
+        mainMenuPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    public void OnBackButton()
+    {
+        optionsPanel.SetActive(false);
+        levelSelectPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void OnQuitButton()
+    {
+        Application.Quit();
+        Debug.Log("Bye Bye...");
+    }
+}
